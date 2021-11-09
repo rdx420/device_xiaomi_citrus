9a6726fa3958d7596a8a3764c3157f9c7d91178d
mobx <67450638+mobxCode@users.noreply.github.com>
2021-11-09 22:36:58 +0700

    Update and rename derp_juice.mk to aosp_juice.mk

--- aosp_juice.mk
+++ aosp_juice.mk
@@ -6,16 +6,17 @@
 
 $(call inherit-product, device/xiaomi/juice/device.mk)
 
-# Inherit some common Derp stuff.
-WITH_GAPPS := true
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+# Inherit some common Aoso stuff.
+WITH_GAPPS := false
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := derp_juice
 PRODUCT_DEVICE := juice
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := sm6115
+PRODUCT_MODEL := SM6115
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
